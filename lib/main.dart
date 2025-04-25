324ad8ae99a31ae1f119e41d01ffc2359051568a
Mohammed3121420 <m.mohammed3121420@gmail.com>
2025-04-25 19:35:34 +0300

    Application title changed

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Your App Name',
+      title: 'Delivery App',
       initialRoute: '/splash',
       routes: {
         '/splash': (context) => SplashScreen(),
